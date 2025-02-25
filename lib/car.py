462a5468441bae9571073dc3ce0afed9286f9ebd
louis-ngugi <louisngugi858@gmail.com>
2025-02-25 09:23:38 +0300

    updated car.py & vehicles.py

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+from car import Car
+c = Car(56, 4)
+print(c.wheel_size)  # Should print 16
+print(c.go())        # Should print "vrrrrrrrooom!"
+print(c.fill_up_tank())  # Should print "filling up!"
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,18 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vroom! vroom! vroom!"
+    
+    def fill_up_tank(self):
+        return "filling up!"
+
+from vehicle import Vehicle
+
+v = Vehicle(18, 4)
+print(v.wheel_size)  # Should print 18
+print(v.go())        # Should print "vrrrrrrrooom!"
+print(v.fill_up_tank())  # Should print "filling up!"
+
